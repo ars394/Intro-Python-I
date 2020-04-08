10073cf3f091ac790e6ebeb89c158020384b8cbd
Anisha Sunkerneni <anishasunkerneni@Anishas-MacBook-Air.local>
2020-04-08 11:22:42 -0700

    anisha: mvp

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,11 +10,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open('foo.txt') as f:
-    read_txt = f.read()
-    print(read_txt)
+foo_file = open('foo.txt')
+read_txt = foo_file.read()
+print(read_txt)
 
-print(f.closed)
+foo_file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
